a3dadf48d74a07300bc00d3bee87674da76c14bd
aman5048 <singhaman5048@gmail.com>
2024-06-27 09:12:28 +0530

    updated 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export { DashBoardPage } from "./Dashbaord/DashBoardPage";
 export { ProductsList } from "./Product/ProductList";
 export { ProductDetail } from "./ProdyctDetails";
 export { CartPage } from "./cart/cartPage";
+export { Register } from "./Register";
 export { OrderPage } from "./orders/orderPage";
 export { Login } from "./Login";
-export { Register } from "./Register";
